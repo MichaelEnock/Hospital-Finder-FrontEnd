e9ae1f45cc3bd9369623842a3dd4ecfbd6805a48
Michael Enock <bsc-15-22@unima.ac.mw>
2025-11-22 20:38:07 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MapContainer from './components/MapContainer'
+
+function App() {
+  return (
+    <div className="w-full justify-center h-screen">
+      <MapContainer />
+      
+    </div>
+  );
+ 
+}
+
+
+export default App
